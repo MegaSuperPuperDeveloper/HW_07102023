565fe982cc76e3ac3fc42fe1d78a92b0609a594c
Мега Крутой <krutoi.chuvak1918@gmail.com>
2023-10-10 19:21:35 +0300

    V1

---
+++ Task43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double Intersection(double b1, double k1, double b2, double k2) { // Функция отправляет значение X.
+    if(k1 - k2 == 0 && b2 - b1 != 0) {
+        Console.WriteLine("Нет Решений!");
+    } else if(k1 - k2 == 0 && b2 - b1 == 0) {
+        return 0;
+    }
+    return (b2 - b1)/(k1 - k2);
+}
+
+Console.Write("Введите первую точку первой прямой: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите вторую точку первой прямой: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первую точку второй прямой: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите вторую точку второй прямой: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+double res = Intersection(b1, k1, b2, k2); 
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({res}, {res})");
